7618fd37f5884e635d74dbeab02a4f88b2835a23
pocha <pocha.sharma@gmail.com>
2014-03-24 17:49:25 +0530

    first commit

--- src/org/codelearn/listviewexample/MainActivity.java
+++ src/org/codelearn/listviewexample/MainActivity.java
@@ -3,8 +3,12 @@ package org.codelearn.listviewexample;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -13,11 +17,17 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		String[] stringArray = new String[] {"List item 1","List item 2","List item 3","List item 4","List item 5"};
-		
     	ArrayAdapter adapter = new CustomAdapter(this);
 		ListView listView = (ListView) findViewById(R.id.listView1);
 		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View listItem, int position, long id) {
+				TextView title = (TextView) listItem.findViewById(R.id.textView1);
+				title.setText("item clicked");
+			}
+		});
 
 	}
 
